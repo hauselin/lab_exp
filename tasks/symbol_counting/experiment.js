c7a0c30ac64542a2b20f43c511389137e62f03e8
hauselin <hauselin@gmail.com>
2020-05-27 09:42:34 -0400

    minor changes

--- tasks/symbol_counting/experiment.js
+++ tasks/symbol_counting/experiment.js
@@ -1,28 +1,28 @@
 var timeline = [];
 
 var welcome = {
-    type: "html-keyboard-response", 
+    type: "html-keyboard-response",
     stimulus: "Welcome to the experiment. Press any key to begin."
 }; timeline.push(welcome);
 
 var instructions = {
     type: "html-keyboard-response",
-    stimulus: "<p>In this experiment, you will be presented with a sequence of " + 
-    "dollar signs ($) and question marks (?). You will need to keep a count of " + 
-    "each of the two types of symbols. There will be 11 trials in total. </p><p> Each symbol will be separated from " +
-    "the next by a fixation cross in the middle of the screen. </p><p> Press any key " +
-    "to begin. </p>"
+    stimulus: "<p>In this experiment, you will be presented with a sequence of " +
+        "dollar signs ($) and question marks (?). You will need to keep a count of " +
+        "each of the two types of symbols. There will be 11 trials in total. </p><p> Each symbol will be separated from " +
+        "the next by a fixation cross in the middle of the screen. </p><p> Press any key " +
+        "to begin. </p>"
 }; timeline.push(instructions);
 
-var test_stimuli = [ 
-    {stimulus: "<div style='font-size:60px;'>$</div>"}, 
-    {stimulus: "<div style='font-size:60px;'>?</div>"}
+var test_stimuli = [
+    { stimulus: "<div style='font-size:60px;'>$</div>" },
+    { stimulus: "<div style='font-size:60px;'>?</div>" }
 ];
 
 var fixation = {
-    type: "html-keyboard-response", 
-    stimulus: "<div style='font-size:60px;'>+</div>",
-    choices: jsPsych.NO_KEYS, 
+    type: "html-keyboard-response",
+    stimulus: "<div style='font-size:30px;'>+</div>",
+    choices: jsPsych.NO_KEYS,
     trial_duration: 500,
 };
 
@@ -34,26 +34,26 @@ var test = {
 };
 
 var test_procedure = {
-    timeline: [fixation, test], 
-    timeline_variables: test_stimuli, 
+    timeline: [fixation, test],
+    timeline_variables: test_stimuli,
     randomize_order: true,
     repetitions: 11
 }; timeline.push(test_procedure);
 
 var debrief_block = {
     type: "html-keyboard-response",
-    stimulus: function() {
-        var dollar_count = jsPsych.data.get().filter({stimulus: "<div style='font-size:60px;'>$</div>"}).count();
-        var question_count = jsPsych.data.get().filter({stimulus: "<div style='font-size:60px;'>?</div>"}).count();
-  
-        return "<p> There were "+dollar_count+" dollar signs ($) and "+question_count+" question marks (?). </p>" + 
-        "<p> Press any key to end the experiment. </p>";
+    stimulus: function () {
+        var dollar_count = jsPsych.data.get().filter({ stimulus: "<div style='font-size:60px;'>$</div>" }).count();
+        var question_count = jsPsych.data.get().filter({ stimulus: "<div style='font-size:60px;'>?</div>" }).count();
+
+        return "<p> There were " + dollar_count + " dollar signs ($) and " + question_count + " question marks (?). </p>" +
+            "<p> Press any key to end the experiment. </p>";
     }
 }; timeline.push(debrief_block);
 
 jsPsych.init({
-    timeline: timeline, 
-    on_finish: function() {
+    timeline: timeline,
+    on_finish: function () {
         jsPsych.data.displayData();
     }
 });
