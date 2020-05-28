d9b1c56e62d53d6d47ba90967ffb06e7290e7f1e
hauselin <hauselin@gmail.com>
2020-05-28 17:16:39 -0400

    change round_step from 25 to 50

--- libraries/utils.js
+++ libraries/utils.js
@@ -18,7 +18,7 @@ function round(value, step = 1.0) {
 
 // generate up to 1000 (but always fewer) values that can be used as inter-trial-intervals (iti)
 // values a sampled from an exponential distribution (default lambda parameter = 4)
-function iti_exponential(low = 300, high = 1000, lambda = 4, round_step = 25) {
+function iti_exponential(low = 300, high = 1000, lambda = 4, round_step = 50) {
   let itis = [];
   for (let i = 0; i <= 1000; i++) { // 
     let iti = Math.log(1 - Math.random()) / (-lambda) * 1000 // multiply by 1000 to convert iti to milliseconds (ms)
