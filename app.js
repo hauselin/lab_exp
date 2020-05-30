48c895b9b6a988742179de528358a0cc2e60ba59
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-05-30 13:30:42 -0400

    Symbol Counting experiment runs locally on port 8080

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express'); 
 var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/tasks'));
+app.use('/jsPsych', express.static(__dirname + "/jsPsych"));
+app.use(express.static(path.join(__dirname, 'tasks/symbol_counting')));
+app.set('symbol_count', __dirname + '/tasks/symbol_counting');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/tasks/symbol_counting/experiment.html'));
+});
+
 var server = app.listen(8080, function() {
     console.log("Server started on port %d", server.address().port);
 });
