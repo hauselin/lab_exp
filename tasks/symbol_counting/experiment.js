dfcc00d684780ba4662644e45f0ae86e9e889e51
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-06-05 23:12:49 -0400

    Display welcome message and instructions

--- tasks/symbol_counting/experiment.js
+++ tasks/symbol_counting/experiment.js
@@ -47,18 +47,18 @@ var timeline = [];
 //     fullscreen_mode: false
 // });
 
-// var welcome = {
-//     type: "html-keyboard-response",
-//     stimulus: "Welcome to the experiment. Press any key to begin."
-// }; timeline.push(welcome);
+var welcome = {
+    type: "html-keyboard-response",
+    stimulus: "Welcome to the experiment. Press any key to begin."
+}; timeline.push(welcome);
 
-// var instructions = {
-//     type: "html-keyboard-response",
-//     stimulus: "<p>In this experiment, you will be presented with a sequence of " +
-//         "dollar signs ($) and question marks (?). <p>You will need to keep a count of " +
-//         "each of the two types of symbols.</p>" +
-//         "<p> Press any key to begin. </p> "
-// }; timeline.push(instructions);
+var instructions = {
+    type: "html-keyboard-response",
+    stimulus: "<p>In this experiment, you will be presented with a sequence of " +
+        "dollar signs ($) and question marks (?). <p>You will need to keep a count of " +
+        "each of the two types of symbols.</p>" +
+        "<p> Press any key to begin. </p> "
+}; timeline.push(instructions);
 
 var symbols = [
     { symbol: "<div style='font-size:80px;'>$</div>" },
