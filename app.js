23cf0da063f9b5e246b372ee9e49a749cae61edf
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-06-09 15:15:32 -0400

    Set up database

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // LOAD MODULES 
 const express = require('express'); 
 const bodyParser = require('body-parser');
-const path = require('path');       // GET THE CURRENT PATH
-const app = express();              // INSTANTIATE THE APP
+const path = require('path');               // GET THE CURRENT PATH
+const app = express();                      // INSTANTIATE THE APP
 
-app.use(bodyParser.json());         // allow app to parse any json request
+app.use(bodyParser.json());                 // allow app to parse any json request
 app.use(express.json());
 
+// TELL EXPRESS TO USE THE FOLLOWING LIBRARIES/FILES
 app.use('/jsPsych', express.static(__dirname + "/jsPsych"));
-app.use(express.static(path.join(__dirname, 'tasks/symbol_counting')));
 app.use('/libraries', express.static(__dirname + "/libraries"));
+app.use(express.static(path.join(__dirname + "/tasks/delay_discount")));
+app.use(express.static(path.join(__dirname + "/tasks/symbol_counting")));
 
+// GET ACCESS TO THE ROUTES DEFINED IN ROUTES.JS
 require('./routes')(app, path)
 
 // START SERVER
