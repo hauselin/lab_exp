67651e0197dd8ee73adfc6237a139da042556451
hauselin <hauselin@gmail.com>
2020-06-11 10:41:53 -0400

    remove old dirs

--- models/SymbolCount.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) =>
-    sequelize.define('SymbolCount', {
-        dollars: DataTypes.INTEGER, 
-        questions: DataTypes.INTEGER,
-        dollars_counted: DataTypes.INTEGER,
-        questions_counted: DataTypes.INTEGER
-    })
