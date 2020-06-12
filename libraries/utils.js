b6ef783de7015e54d19e73268e349df073054590
hauselin <hauselin@gmail.com>
2020-06-12 10:33:31 -0400

    clean up number_update

--- libraries/utils.js
+++ libraries/utils.js
@@ -57,29 +57,27 @@ var difficulty_steps = combine(difficulty_min_max, symbols_min_max);
 difficulty_steps
 
 // generate mental math updating array
-function number_update(array1, array2, n_distractors){
+function number_update(array1, array2, n_distractors) {
     var output = [];
     var correct = '';
+    // determine correct response
     for (i = 0; i < array1.length; i++) {
-        if (i < array2.length){
+        if (array2.length < array1.length) {
+            var correct_num = array1[i] + array2[0]; // if array2 is shorter than array1, always add the first element of array2 to each element in array1
+        } else if (array1.length == array2.length) {
             var correct_num = array1[i] + array2[i];
-            if (correct_num < 0) {
-                correct_num = 0
-            }
-            correct = correct.concat(correct_num.toString());
-        } else {
-            var correct_num = array1[i] + array2[array2.length - 1];
-            if (correct_num < 0) {
-                correct_num = 0
-            }
-            correct = correct.concat(correct_num.toString());
         }
+        if (correct_num < 0) {
+            correct_num = 0; // TODO: fix algorithm
+        }
+        correct = correct.concat(correct_num.toString()); // concat string integers
     }
     output.push(correct);
+    // create distractors/wrong responses
     for (i = 0; i < n_distractors; i++) {
         var distractor = '';
         for (j = 0; j < correct.length; j++) {
-            var distractor_num = Number(correct.charAt(j)) + (Math.floor(Math.random() * 3) + -1);
+            var distractor_num = Number(correct.charAt(j)) + (Math.floor(Math.random() * 3) + -1); // hm? next time explain to me the logic of this?
             if (distractor_num < 0) {
                 distractor_num = Number(correct.charAt(j)) + (Math.floor(Math.random() * 2));
             }
