db443f1a3148f8abeae5665f718c62c26a719bf7
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-06-12 21:03:01 -0400

    Create DataController.js to help clean routes

---
+++ DataController.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/jspsych', 
+{ useUnifiedTopology: true, useNewUrlParser: true });
+
+var symbolSchema = new mongoose.Schema({}, { strict: false });
+var DataCollection = mongoose.model('DataCollection', symbolSchema);
+
+module.exports = {
+    create(req, res) {
+        DataCollection.create({
+            subject: req.body[0].subject,
+            task: req.body[0].task,
+            experiment: req.body[0].experiment,
+            condition: req.body[0].condition,
+            browser: req.body[0].browser,
+            datetime: req.body[0].datetime,
+            data: req.body
+        })
+    }
+}
