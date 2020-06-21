bb6c258325bd9b94d5754daa42084055bc9ee87e
hauselin <hauselin@gmail.com>
2020-06-21 19:50:17 -0400

    added comments to routes.js

--- routes.js
+++ routes.js
@@ -20,8 +20,15 @@ module.exports = function (app, path) {
     app.get('/*', function (req, res) {
         var params = req.params;
         console.log(params);
-        res.render("test.ejs", { my_variable: params['0'] }); // looks within views dir for ejs files
-    })
+        var var_to_display;
+        if (params['0'] == '') {
+            var_to_display = "you didn't specify a route; see examples below";
+        } else {
+            var_to_display = params['0'];
+        }
+        // eventually, we'll pass in data from our database to our ejs file
+        res.render("test.ejs", { my_variable: var_to_display }); // looks within views dir for ejs files
+    });
 
     // POST REQUESTS
     app.post('/submit-data', DataController.create);
