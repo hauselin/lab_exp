41a851a442d190f91f36ff77c2ddccde4a502fa5
hauselin <hauselin@gmail.com>
2020-06-21 19:34:02 -0400

    add views/test.ejs and add a get request in routes.js to test test.ejs

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const DataController = require('./DataController');
 
-module.exports = function(app, path) {
+module.exports = function (app, path) {
 
     // GET REQUESTS
     // app.get('/delay-discounting', function(req, res) {
@@ -9,13 +9,19 @@ module.exports = function(app, path) {
     // app.get('/symbol-counting', function(req, res) {
     //     res.sendFile(path.join(__dirname + '/tasks/symbol_counting/experiment.html'));
     // });
-    app.get('/home', function(req, res) {
+    app.get('/home', function (req, res) {
         res.sendFile(path.join(__dirname + '/index.html'));
     });
-    app.get('/grit_short', function(req, res) {
+    app.get('/grit_short', function (req, res) {
         res.sendFile(path.join(__dirname + '/surveys/grit_short.html'))
-    })
+    });
 
+    // catch-all route to demonstrate/test ejs file
+    app.get('/*', function (req, res) {
+        var params = req.params;
+        console.log(params);
+        res.render("test.ejs", { my_variable: params['0'] }); // looks within views dir for ejs files
+    })
 
     // POST REQUESTS
     app.post('/submit-data', DataController.create);
