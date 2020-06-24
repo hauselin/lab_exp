d005b52d309d2e25a294f9b8dac13f5c7c8748f3
hauselin <hauselin@gmail.com>
2020-06-23 23:16:29 -0400

    reverse code iff data.reverse == 1; and several minor tweaks/additions

--- surveys/grit_short.js
+++ surveys/grit_short.js
@@ -1,71 +1,81 @@
-var task = 'grit_short';
+var task = 'grit_short'; // must be identical to script name and csv file name
 var slider_width = 500; // width of slider in pixels
 var scale_min_max = [1, 5]; // slider min max values
 var scale_starting_points = [2, 3, 4]; // starting point of scale; if length > 1, randomly pick one for each scale item
 var scale_labels = ['not at all like me', 'very much like me']
 var step = 0.01; // step size of scale
 var require_movement = false; // whether subject must move slider before they're allowed to click continue
-
+var shuffle_items = false; // randomize order of item presentation
+var debug = true;
 
 // read survey csv file
 // https://www.papaparse.com
-var survey;
 Papa.parse('../surveys/' + task + '.csv', {
-    download: true, 
-    header: true, 
+    download: true,
+    header: true,
     dynamicTyping: true,
-    complete: function (results) { 
-
-        survey = results.data;
-
-        jsPsych.data.addProperties({
-            task: task,
-            browser: navigator.userAgent, // browser info
-            datetime: Date(),
-        });
-
-        var grit_procedure = {
-            timeline: [
-                {
-                    type: 'html-slider-response',
-                    stimulus: jsPsych.timelineVariable('desc'),
-                    data: {
-                        q: jsPsych.timelineVariable('q'),
-                        subscale: jsPsych.timelineVariable('subscale'), 
-                        reverse: jsPsych.timelineVariable('reverse')
-                    },
-                    labels: scale_labels,
-                    slider_width: slider_width,
-                    min: scale_min_max[0],
-                    max: scale_min_max[1],
-                    start: function () { return jsPsych.randomization.sampleWithoutReplacement(scale_starting_points, 1)[0] },
-                    step: step,
-                    require_movement: require_movement,
-                    on_finish: function(data) {
-                        data.response = Number(data.response)
-                        data.resp_reverse = scale_min_max[1] + 1 - data.response
-                    }
-                }
-            ],
-        
-            timeline_variables: survey
-        };
-        
-        jsPsych.init({
-            timeline: [grit_procedure],
-            on_finish: function () {
-                jsPsych.data.displayData();
-                jsPsych.data.addProperties({ total_time: jsPsych.totalTime() });
-                $.ajax({
-                    type: "POST",
-                    url: "/submit-data",
-                    data: jsPsych.data.get().json(),
-                    contentType: "application/json"
-                })
-                jsPsych.data.displayData();
-            }
-        })
-        
+    complete: function (results) {
+        run_survey(results.data)
     }
 });
 
+// entire task is a (callback) function called by Papa.parse
+function run_survey(survey) {
+    jsPsych.data.addProperties({
+        task: task,
+        browser: navigator.userAgent, // browser info
+        datetime: Date(),
+    });
+
+    var start_point;
+    var procedure = {
+        timeline: [{
+            type: 'html-slider-response',
+            stimulus: jsPsych.timelineVariable('desc'),
+            data: {
+                q: jsPsych.timelineVariable('q'),
+                subscale: jsPsych.timelineVariable('subscale'),
+                reverse: jsPsych.timelineVariable('reverse')
+            },
+            labels: scale_labels,
+            slider_width: slider_width,
+            min: scale_min_max[0],
+            max: scale_min_max[1],
+            start: function () {
+                start_point = jsPsych.randomization.sampleWithoutReplacement(scale_starting_points, 1)[0];
+                return start_point;
+            },
+            step: step,
+            require_movement: require_movement,
+            on_finish: function (data) {
+                data.start_point = start_point;
+                data.resp = Number(data.response);
+                data.resp_reverse = data.resp;
+                if (data.reverse) { // reverse code item if necessary
+                    data.resp_reverse = scale_min_max[1] + 1 - data.resp;
+                }
+                if (debug) {
+                    console.log("q" + data.q + " (reverse: " + data.reverse + "): " + data.stimulus);
+                    console.log("resp: " + data.resp + ", resp_reverse: " + data.resp_reverse);
+                }
+            }
+        }],
+        timeline_variables: survey,
+        randomize_order: shuffle_items
+    };
+
+    jsPsych.init({
+        timeline: [procedure],
+        on_finish: function () {
+            jsPsych.data.displayData();
+            jsPsych.data.addProperties({ total_time: jsPsych.totalTime() });
+            $.ajax({
+                type: "POST",
+                url: "/submit-data",
+                data: jsPsych.data.get().json(),
+                contentType: "application/json"
+            })
+            jsPsych.data.displayData();
+        }
+    });
+};
