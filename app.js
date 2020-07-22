08795bdf5c9e79fdcc2a35cd5f74f307ca9e581a
hauselin <hauselin@gmail.com>
2020-07-21 22:39:38 -0400

    minor edits

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(function (req, res) {
 // TODO Frank: same as above
 // Handle 500
 app.use(function (error, req, res, next) {
-    res.send('500: Internal Server Error', 500);
+    res.render("404.ejs");
 });
 
 // START SERVER
