e7598d6243bf307d4d299cdc1f33fd774edb55e5
Frank <frank_fan_1201@outlook.com>
2020-07-29 03:34:20 -0700

    Query data.

--- routes/viz.js
+++ routes/viz.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var DataLibrary = require("../models/datalibrary")
 
 router.get("/:type/:uniquestudyid/viz", function (req, res) {
-    const file = 'viz/' + req.params.uniquestudyid + '.ejs';
-    res.render(file); // render {uniquestudyid}.ejs in views directory
+    Promise.all([
+        DataLibrary.find({uniquestudyid:req.params.uniquestudyid}).lean(),
+    ])
+        .then(([data]) => {
+            console.log(data);
+            const file = 'viz/' + req.params.uniquestudyid + '.ejs';
+            res.render(file, {data: data}); // render {uniquestudyid}.ejs in views directory
+        })
 });
 
 module.exports = router;
