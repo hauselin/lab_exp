065317c7336693bc62bbf69b3d5f1ad11629d05a
Frank <frank_fan_1201@outlook.com>
2020-08-12 03:45:51 -0400

    Update utils.js

--- libraries/utils.js
+++ libraries/utils.js
@@ -340,6 +340,7 @@ function add_ip_info(info_) {
         info_.city = geoplugin_city();
         info_.region = geoplugin_region();
         info_.country_name = geoplugin_countryName();
+        info_.country_code = geoplugin_countryCode();
         info_.latitude = geoplugin_latitude();
         info_.longitude = geoplugin_longitude();
     } catch (err) {
