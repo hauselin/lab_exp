c2a9a6e51044d9b9ae1754a646a498f45298eba0
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-08-12 22:39:06 -0400

    Delete documents by year and month for task complete

--- routes/delete.js
+++ routes/delete.js
@@ -45,7 +45,7 @@ router.get('/:type/:uniquestudyid/delete/:yyyy', function (req, res) {
         { sort: { time: -1 } }).lean()
         .then(doc => {
             if (doc.length == 0) {
-                console.log("No documents found for the year " + req.params.yyyy + ".");
+                console.log("No documents found for the year " + req.params.yyyy + " to delete.");
             } else {
                 for (var i = 0; i < doc.length; i++) {
                     DataLibrary.deleteOne( {user_time: doc[i].user_time}, function(err) {
@@ -56,6 +56,32 @@ router.get('/:type/:uniquestudyid/delete/:yyyy', function (req, res) {
                 }; console.log("Deleted record(s) for " + req.params.yyyy + " successfully.");
             }
         });
+});
+
+
+router.get('/:type/:uniquestudyid/delete/:yyyy/:mm', function (req, res) {
+    // Filter and download documents by year and month for a given task
+    DataLibrary.find(
+        {
+            uniquestudyid: req.params.uniquestudyid,
+            "utc_date.year": Number(req.params.yyyy),
+            "utc_date.month": Number(req.params.mm)
+        },
+        {},
+        { sort: { time: -1 } }).lean()
+        .then(doc => {
+            if (doc.length == 0) {
+                console.log("No documents found to delete.");
+            } else {
+                for (var i = 0; i < doc.length; i++) {
+                    DataLibrary.deleteOne( {user_time: doc[i].user_time}, function(err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                }; console.log("Deleted record(s) for " + req.params.yyyy + "/" + req.params.mm + " successfully.");
+            }
+        })
 });
 
 module.exports = router;
