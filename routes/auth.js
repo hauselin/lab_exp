9439cd6f17a9f6843656187abee3ee80917870d5
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-09-03 12:44:13 -0400

    Change req to res in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ router.post("/login", passport.authenticate("local",
 
 router.get("/logout", function(req, res) {
     req.logout();
-    req.redirect("/");
+    res.redirect("/");
 });
 
 module.exports = router;
