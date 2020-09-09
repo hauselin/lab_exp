1f327b71a589dc9fb2ce6b6d4c2af090ec3ba6e2
Frank <frank_fan_1201@outlook.com>
2020-09-09 13:32:55 -0400

    Change grit survey data storing structure and viz queries.

--- routes/viz.js
+++ routes/viz.js
@@ -85,54 +85,39 @@ router.get("/tasks/stroop/viz", function (req, res) {
 
 router.get("/surveys/gritshort/viz", function (req, res) {
     DataLibrary.find({ uniquestudyid: 'gritshort' }, {}, { sort: { time: -1 } }).lean().then(data => {
-        const keys2select = ['subject', 'uniquesubjectid', 'trial_type', 'subscale', 'resp_reverse', 'country', 'country_code', 'longitude', 'latitude', 'time'];  // columns/keys to select
 
         var data_array = [];
-        var subject_array = [];
         data.map(function (i) {  // map/loop through each document to get relevant data
-            const temp_data = i.data; // get jspsych data
-            var data_subset = temp_data.filter(s => s.trial_type != 'external-html');  // select relevant rows
-            var data_subset = data_subset.map(s => helper.pick(s, keys2select));  // select relevant columns
-            data_subset.forEach(function (s) { // for each row in this document
-                s.country_id = Number(iso_countries.alpha2ToNumeric(s.country_code))  // get country code
+            var temp_data = i.datasummary_; // get datasummary_
+            // convert country code to country id
+            temp_data.forEach(function (s) {
+                s.country_id = Number(iso_countries.alpha2ToNumeric(s.country_code))
             })
-            data_by_subscale = d3.rollup(data_subset, function (v) {
-                return {
-                    mean_resp: d3.mean(v, d => d.resp_reverse),  // mean response
-                    subject: d3.min(v, d => d.subject),  // get subject ID
-                    time: d3.min(v, d => d.time)  // get start time
-                }
-            }, d => d.subscale)
-            subscale_array = Array.from(data_by_subscale, function (i) {  // unnest data
-                return { subscale: i[0], resp_reverse: i[1].mean_resp, subject: i[1].subject, time: i[1].time }
-            })
-            subject_array.push(subscale_array);
-            data_array.push(data_subset);
+            data_array.push(temp_data);
         });
         data_array = data_array.flat(1);  // flatten objects in array
-        subject_array = subject_array.flat(1);  // flatten objects in array
-        // console.log(data_array);
-        // console.log(data_array.length);
-        console.log(subject_array);
-        console.log(subject_array.length);
+        console.log(data_array)
 
         // prepare data for chloropleth
-        country_data = d3.rollups(data_array, // compute median for each country
+        // compute median rt interference for each country
+        var temp_data = data_array.filter(x => x.type == "all");
+        // console.log(temp_data)
+        country_data = d3.rollups(temp_data,
             function (v) {
                 return {
-                    mean_resp: d3.mean(v, d => d.resp_reverse),  // mean response
-                    country_name: d3.min(v, d => d.country)  // get country name
+                    mean_resp: d3.mean(v, d => d.value),  // median rt interference
+                    country_name: d3.min(v, d => d.country_name)  // get country name
                 }
             },
             d => d.country_id);  // by country id
-        // console.log(country_data);  // nested data
+        console.log(country_data);  // nested data
         country_data = Array.from(country_data, function (i) {  // unnest data
             return { country_id: i[0], country_name: i[1].country_name, mean_resp: i[1].mean_resp }
         })
-        // console.log(country_data);
+        console.log(country_data);
 
         // render
-        res.render('viz/gritshort.ejs', { data_array: data_array, subject_array: subject_array, country_array: country_data });
+        res.render('viz/gritshort.ejs', { data_array: data_array, country_array: country_data });
     }).catch(err => {
         console.log(err);
         res.status(500).send(err);
