f3401d70b82b95dd25a7e9ea6d271faf5a43f7ea
Maham Khan <mahamkh.khan@mail.utoronto.ca>
2020-10-06 14:14:16 -0400

    Add comments to index.js

--- routes/index.js
+++ routes/index.js
@@ -97,8 +97,8 @@ router.get('/', function (req, res) {
         studies: d3.shuffle(page_elements.studies)
     })
     
-    if (process.env.CONNECT == "f") { // NODE_ENV is undefined
-        var header = JSON.stringify(req.headers)["host"]; 
+    if (process.env.CONNECT == "f") { // CONNECT is set to f for false at first 
+        var header = JSON.stringify(req.headers)["host"]; // returns a json of the header, we use ["host"] to retrieve domain
         if (header == "localhost:8080") {
             process.env.NODE_ENV = "development";
             var mongoDB = process.env.MONGODB_URI || "mongodb://localhost/datalibrary"; 
@@ -111,7 +111,7 @@ router.get('/', function (req, res) {
             if (err) { console.log('Not connected to database!'); } else {
                 console.log('Successfully connected to database.')
         }});
-        process.env.CONNECT = "t";
+        process.env.CONNECT = "t"; // set CONNECT to t to show that it is connected to a db
     };
 
 });
