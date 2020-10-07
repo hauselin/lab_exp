70643ce5901d24392767d397a66cbe96beb17826
hauselin <hauselin@gmail.com>
2020-10-06 21:39:18 -0400

    frontend changes

--- public/page_elements.js
+++ public/page_elements.js
@@ -3,25 +3,25 @@ var surveys = [
         uniquestudyid: 'bigfiveaspect',
         name: 'Big-five personality traits',
         description: 'Are you open, conscientious, extraverted, agreeable, or neurotic?',
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         uniquestudyid: 'gritshort',
         name: 'How gritty are you?',
         description: "Are you good at pursuing long-term goals?",
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         uniquestudyid: 'bigfiveaspect',
         name: 'Big-five personality traits',
         description: 'Are you open, conscientious, extraverted, agreeable, or neurotic?',
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         uniquestudyid: 'gritshort',
         name: 'How gritty are you?',
         description: "Are you good at pursuing long-term goals?",
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     
 ];
@@ -31,25 +31,25 @@ var tasks = [
         uniquestudyid: 'delaydiscount',
         name: 'Money now or money later?',
         description: 'Do you prefer immediate or future, delayed rewards? Learn about how you delay instant gratification.',
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         uniquestudyid: 'stroop',
         name: 'Attention and cognitive control',
         description: 'How good are you at controlling your attention? Try this simple Stroop color-word naming task to find out.',
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         uniquestudyid: 'symbolcount',
         name: 'Brain power and working memory',
         description: 'How good is your working memory? Challenge yourself with this symbol counting task.',
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         uniquestudyid: 'updatemath',
         name: 'Working memory and mathematics abilities',
         description: 'Coming soon',
-        time: '3-5 mins'
+        time: '3-5 min'
     }
 ];
 
@@ -58,7 +58,7 @@ var studies = [
         uniquestudyid: 'studyA',
         name: 'Cognitive effort',
         description: 'Coming soon',
-        time: '3-5 mins'
+        time: '3-5 min'
     }
 ];
 
@@ -79,17 +79,17 @@ var tasks_to_try = [
     {
         route: '/tasks/delaydiscount',
         uniquestudyid: 'delaydiscount',
-        time: '3-5 mins'
+        time: '3-5 min'
     },
     {
         route: '/surveys/gritshort',
         uniquestudyid: 'gritshort',
-        time: '1-3 mins'
+        time: '1-3 min'
     },
     {
         route: '/tasks/stroop',
         uniquestudyid: 'stroop',
-        time: '5-8 mins'
+        time: '5-8 min'
     },
 ];
 
