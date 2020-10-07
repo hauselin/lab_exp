6e8b89c9754207214f2e52d924f27020a9f94a0a
hauselin <hauselin@gmail.com>
2020-10-06 22:14:53 -0400

    comment out viz route code

--- routes/viz.js
+++ routes/viz.js
@@ -88,66 +88,66 @@ router.get("/tasks/stroop/viz", function (req, res) {
 router.get("/surveys/gritshort/viz", function (req, res) {
     DataLibrary.find({ uniquestudyid: 'gritshort' }, {}, { sort: { time: -1 } }).lean().then(data => {
 
-        var data_array2 = [];
-        var data_array = [];
-        data.map(function (i) {
-            var temp_data = i.datasummary;
-            temp_data.forEach(function (s) {
-                s.country_id = Number(iso_countries.alpha2ToNumeric(s.country_code))
-            })
-            data_array.push(temp_data);
+        // var data_array2 = [];
+        // var data_array = [];
+        // data.map(function (i) {
+        //     var temp_data = i.datasummary;
+        //     temp_data.forEach(function (s) {
+        //         s.country_id = Number(iso_countries.alpha2ToNumeric(s.country_code))
+        //     })
+        //     data_array.push(temp_data);
 
-            var temp_data2 = i.data;
-            temp_data2 = temp_data2.filter(x => x.trial_type == 'html-slider-response')
-            // console.log(temp_data2);
-            subject_map = d3.rollups(temp_data2,
-                function (v) {
-                    return {
-                        subject: d3.min(v, d => d.subject),
-                        time: d3.min(v, d => d.time),
-                        value: d3.mean(v, d => d.resp_reverse),
-                        country_name: d3.min(v, d => d.country),
-                        country_code: d3.min(v, d => d.country_code),
-                        country_id: d3.min(v, d => Number(iso_countries.alpha2ToNumeric(d.country_code)))
-                    }
-                },
-                d => d.subscale);
-            all_subscales = [
-                'all',
-                {
-                    subject: subject_map[0][1].subject,
-                    time: subject_map[0][1].time,
-                    value: (subject_map[0][1].value + subject_map[1][1].value) / 2,
-                    country_name: subject_map[0][1].country_name,
-                    country_code: subject_map[0][1].country_code,
-                    country_id: subject_map[0][1].country_id
-                }
-            ];
-            subject_map.push(all_subscales);
-            data_array2.push(subject_map);
-        });
-        data_array = data_array.flat(1);
-        data_array2 = data_array2.flat(1);
+        //     var temp_data2 = i.data;
+        //     temp_data2 = temp_data2.filter(x => x.trial_type == 'html-slider-response')
+        //     // console.log(temp_data2);
+        //     subject_map = d3.rollups(temp_data2,
+        //         function (v) {
+        //             return {
+        //                 subject: d3.min(v, d => d.subject),
+        //                 time: d3.min(v, d => d.time),
+        //                 value: d3.mean(v, d => d.resp_reverse),
+        //                 country_name: d3.min(v, d => d.country),
+        //                 country_code: d3.min(v, d => d.country_code),
+        //                 country_id: d3.min(v, d => Number(iso_countries.alpha2ToNumeric(d.country_code)))
+        //             }
+        //         },
+        //         d => d.subscale);
+        //     all_subscales = [
+        //         'all',
+        //         {
+        //             subject: subject_map[0][1].subject,
+        //             time: subject_map[0][1].time,
+        //             value: (subject_map[0][1].value + subject_map[1][1].value) / 2,
+        //             country_name: subject_map[0][1].country_name,
+        //             country_code: subject_map[0][1].country_code,
+        //             country_id: subject_map[0][1].country_id
+        //         }
+        //     ];
+        //     subject_map.push(all_subscales);
+        //     data_array2.push(subject_map);
+        // });
+        // data_array = data_array.flat(1);
+        // data_array2 = data_array2.flat(1);
 
-        data_array2 = Array.from(data_array2, function (i) {
-            return { type: i[0], subject: i[1].subject, value: i[1].value, time: i[1].time, country_name: i[1].country_name, country_code: i[1].country_code, country_id: i[1].country_id, }
-        })
+        // data_array2 = Array.from(data_array2, function (i) {
+        //     return { type: i[0], subject: i[1].subject, value: i[1].value, time: i[1].time, country_name: i[1].country_name, country_code: i[1].country_code, country_id: i[1].country_id, }
+        // })
 
-        var temp_data = data_array.filter(x => x.type == "all");
-        // console.log(temp_data)
-        country_array = d3.rollups(temp_data,
-            function (v) {
-                return {
-                    mean_resp: d3.mean(v, d => d.value),
-                    country_name: d3.min(v, d => d.country_name)
-                }
-            },
-            d => d.country_id);
-        // console.log(country_array);
-        country_array = Array.from(country_array, function (i) {
-            return { country_id: i[0], country_name: i[1].country_name, mean_resp: i[1].mean_resp }
-        })
-        // console.log(country_array);
+        // var temp_data = data_array.filter(x => x.type == "all");
+        // // console.log(temp_data)
+        // country_array = d3.rollups(temp_data,
+        //     function (v) {
+        //         return {
+        //             mean_resp: d3.mean(v, d => d.value),
+        //             country_name: d3.min(v, d => d.country_name)
+        //         }
+        //     },
+        //     d => d.country_id);
+        // // console.log(country_array);
+        // country_array = Array.from(country_array, function (i) {
+        //     return { country_id: i[0], country_name: i[1].country_name, mean_resp: i[1].mean_resp }
+        // })
+        // // console.log(country_array);
 
         // render
         // res.render('viz/gritshort.ejs', { data_array: data_array2, country_array: country_array });
