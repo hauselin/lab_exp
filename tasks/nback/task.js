221308d0192bd2a462ffa865295dad1e0769146a
Frank <frank_fan_1201@outlook.com>
2020-12-04 18:11:34 -0800

    Generate scenes timeline.

--- tasks/nback/task.js
+++ tasks/nback/task.js
@@ -1,6 +1,7 @@
 const image_link = '1.gif';
 const num_back = 2;
 const grid_size = 3;
+const n_trials = 3;
 
 function update_array(array, new_entry) {
     array.shift();
@@ -9,8 +10,9 @@ function update_array(array, new_entry) {
 }
 
 function update_scene(matrix, coords) {
-    matrix[coords[1], coords[0]] = image_link;
-    return matrix
+    var new_matrix = jsPsych.utils.deepCopy(matrix);
+    new_matrix[coords[1]][coords[0]] = image_link;
+    return new_matrix
 }
 
 var scene = [];
@@ -18,15 +20,17 @@ for (var i = 0; i < grid_size; i++) {
     scene[i] = new Array(grid_size).fill(0);
 }
 
+var scenes = [];
+for (var i = 0; i < n_trials; i++) {
+    tile_x = Math.floor(Math.random() * (grid_size));
+    tile_y = Math.floor(Math.random() * (grid_size));
+    console.log([tile_x, tile_y]);
+    scenes.push(update_scene(scene, [tile_x, tile_y]));
+}
+
 var trial = {
     type: 'html-keyboard-response',
     stimulus: jsPsych.plugins['vsl-grid-scene'].generate_stimulus(scene, [100, 100]),
-    on_start: function () {
-        tile_x = Math.floor(Math.random() * (grid_size));
-        tile_y = Math.floor(Math.random() * (grid_size));
-        console.log([tile_x, tile_y]);
-        scene = update_scene(scene, [tile_x, tile_y]);
-    },
 }
 
 var trials = {
