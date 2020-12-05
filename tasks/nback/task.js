cc089cd72b253acdb2c5c5d1cf1928ce9c2ba3e3
Frank <frank_fan_1201@outlook.com>
2020-12-04 17:49:46 -0800

    Mordularize.

--- tasks/nback/task.js
+++ tasks/nback/task.js
@@ -1,5 +1,6 @@
 const image_link = '1.gif';
 const num_back = 2;
+const grid_size = 3;
 
 function update_array(array, new_entry) {
     array.shift();
@@ -7,25 +8,17 @@ function update_array(array, new_entry) {
     return array
 }
 
-var scene =
-    [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ];
+var scene = [];
+for (var i = 0; i < grid_size; i++) {
+    scene[i] = new Array(grid_size).fill(0);
+}
 
 var trial = {
-    type: 'vsl-grid-scene',
-    stimuli: scene,
-    image_size: [100, 100]
-};
-
-var trial_2 = {
     type: 'html-keyboard-response',
     stimulus: jsPsych.plugins['vsl-grid-scene'].generate_stimulus(scene, [100, 100])
 }
 
 jsPsych.init({
-    timeline: [trial, trial_2],
+    timeline: [trial],
     on_finish: function () { jsPsych.data.displayData(); }
 });
