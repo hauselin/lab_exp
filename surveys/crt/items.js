4a3faa7dae679619ecfeabdf1ba2601e82ac1118
hauselin <hauselin@gmail.com>
2021-02-15 20:51:59 -0600

    add crt survey

---
+++ surveys/crt/items.js
@@ -0,0 +1,82 @@
+var items = [
+    {
+        "q": 1,
+        "desc": "The ages of Mark and Adam add up to 28 years total. Mark is 20 years older than Adam. How many years old is Adam?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "4",
+        "answer_intuitive": "8"
+    },
+    {
+        "q": 2,
+        "desc": "If it takes 10 seconds for 10 printers to print out 10 pages of paper, how many seconds will it take 50 printers to print out 50 pages of paper?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "10",
+        "answer_intuitive": "50"
+    },
+    {
+        "q": 3,
+        "desc": "On a loaf of bread, there is a patch of mold. Every day, the patch doubles in size. If it takes 40 days for the patch to cover the entire loaf of bread, how many days would it take for the patch to cover half of the loaf of bread?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "39",
+        "answer_intuitive": "20"
+    },
+    {
+        "q": 4,
+        "desc": "If you’re running a race and you pass the person in second place, what place are you in?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "2",
+        "answer_intuitive": "1"
+    },
+    {
+        "q": 5,
+        "desc": "A farmer had 15 sheep and all but 8 died. How many are left?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "8",
+        "answer_intuitive": "7"
+    },
+    {
+        "q": 6,
+        "desc": "Emily’s father has three daughters. The first two are named April and May. What is the third daughter’s name?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "Emily",
+        "answer_intuitive": "June"
+    },
+    {
+        "q": 7,
+        "desc": "Like all animals, cows need to eat and drink. What do adult cows generally drink?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "water",
+        "answer_intuitive": "milk"
+    },
+    {
+        "q": 8,
+        "desc": "A bat and a ball cost $1.10 in total. The bat costs $1.00 more than the ball. How many dollars does the ball cost?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "0.05",
+        "answer_intuitive": "0.10"
+    },
+    {
+        "q": 9,
+        "desc": "If it takes 5 machines 5 minutes to make 5 widgets, how long would it take 100 machines to make 100 widgets?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "100",
+        "answer_intuitive": "5"
+    },
+    {
+        "q": 10,
+        "desc": "In a lake, there is a patch of lily pads. Every day, the patch doubles in size. If it takes 48 days for the patch to cover the entire lake, how long would it take for the patch to cover half of the lake?",
+        "subscale": "crt",
+        "reverse": 0,
+        "answer_correct": "47",
+        "answer_intuitive": "24"
+    }
+];
