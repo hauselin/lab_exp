7364d4ee02f37273db5d6330065837c441fe1852
Frank <frank_fan_1201@outlook.com>
2021-04-24 12:46:24 -0400

    Create 3 dot motion trials.

--- studies/efforttrain/task.js
+++ studies/efforttrain/task.js
@@ -2,14 +2,24 @@ var font_colour = "white";
 var background_colour = "black";
 set_colour(font_colour, background_colour);
 
+var debug = true;
+
 trial_repetitions = 5;
 rocket_selection_deadline = null; // ms
 
+// dot motion task parameters
+dot_motion_repetitions = 3;
+p_incongruent_dots = 0.65;
+num_distractors = 30;
+num_answer = 300;
+distractor_coherence = 0.75;
+answer_coherence = 0.75;
+
 // colours used for task, with left and right randomized for each experiment
 colours = ['red', 'blue', 'green', 'yellow'];
-var colours = jsPsych.randomization.repeat(colours, 1);
-colours_left = colours.slice(2, 4)
-colours_right = colours.slice(0, 2)
+var colours_shuffled = jsPsych.randomization.repeat(colours, 1);
+colours_left = colours_shuffled.slice(2, 4)
+colours_right = colours_shuffled.slice(0, 2)
 
 var subject_id = 1;
 var assigned_info = assign.filter(i => i.subject == subject_id)[0];
@@ -118,11 +128,12 @@ var dot_motion = {
     background_color: background_colour,
     choices: [37, 39],
     trial_duration: 10000,
-    coherence: [0.1, 1.0],
-    dot_color: ["blue", "red"],
+    coherence: [jsPsych.timelineVariable('left_coherence'), jsPsych.timelineVariable('right_coherence')],
+    dot_color: [jsPsych.timelineVariable('left_colour'), jsPsych.timelineVariable('right_colour')],
+    correct_choice: [jsPsych.timelineVariable('correct_choice')],
     move_distance: 6,
     number_of_apertures: 2,
-    number_of_dots: [50, 200],
+    number_of_dots: [jsPsych.timelineVariable('left_num'), jsPsych.timelineVariable('right_num')],
     RDK_type: 2,
     aperture_width: 500,
     aperture_center_x: [(window.innerWidth / 2), (window.innerWidth / 2)],
@@ -132,14 +143,63 @@ var dot_motion = {
     }
 }
 
+// 3 dot motion trials in a sequence
+function hard_task_timeline_variables() {
+    var timeline_variables = []
+    for (i = 0; i < dot_motion_repetitions; i++) {
+        var selected_colours = jsPsych.randomization.sampleWithoutReplacement(colours, 2);
+        var answer = selected_colours[0];
+        var distractor = selected_colours[1];
+        var answer_moves_left = {
+            left_colour: distractor,
+            right_colour: answer,
+            left_num: num_distractors,
+            right_num: num_answer,
+            left_coherence: distractor_coherence,
+            right_coherence: answer_coherence,
+            correct_choice: 39,
+        };
+        var answer_moves_right = {
+            left_colour: answer,
+            right_colour: distractor,
+            left_num: num_answer,
+            right_num: num_distractors,
+            left_coherence: answer_coherence,
+            right_coherence: distractor_coherence,
+            correct_choice: 37,
+        };
+        if (p_incongruent_dots < Math.random()) { // if incongruent
+            if (colours_left.includes(answer)) {  // if answer is a left colour
+                var trial_variable = answer_moves_right;
+            } else {
+                var trial_variable = answer_moves_left;
+            }
+        } else {
+            if (0.5 < Math.random()) {
+                var trial_variable = answer_moves_left;
+            } else {
+                var trial_variable = answer_moves_right;
+            }
+        }
+        timeline_variables.push(trial_variable);
+    }
+    return timeline_variables;
+};
+
+console.log(hard_task_timeline_variables());
+
+var dot_motion_trials = {
+    timeline: [dot_motion],
+    timeline_variables: hard_task_timeline_variables()
+}
 
 // TODO: 3 blocks: pre-training, training, post-training
 // pre-training = post-training -> no feedback for correctness
 // no data for post-training
 // store dot motion acc, correct rt, num correct
 // training -> feedback with aliens
-var rockets_procedure = {
-    timeline: [rockets, rocket_chosen, dot_motion],
+var pre_training = {
+    timeline: [rockets, rocket_chosen, dot_motion_trials],
     repetitions: trial_repetitions,
 }
 
@@ -148,7 +208,7 @@ var rockets_procedure = {
 var timeline = []
 // timeline.push(instructions);
 timeline.push(colour_blocks);
-timeline.push(rockets_procedure);
+timeline.push(pre_training);
 
 
 
