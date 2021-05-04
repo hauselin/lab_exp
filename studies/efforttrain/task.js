31dbcf1a40d5d41b62b57127e9f98c9631d281cd
hauselin <hauselin@gmail.com>
2021-05-03 22:58:54 -0600

    minor changes to effort train

--- studies/efforttrain/task.js
+++ studies/efforttrain/task.js
@@ -4,24 +4,28 @@ set_colour(font_colour, background_colour);
 
 var debug = true;
 
-trial_repetitions = 5;
-rocket_selection_deadline = null; // ms
+const trial_repetitions = 5;
+const rocket_selection_deadline = null; // ms
+
+var rnorm = new Ziggurat();  // rnorm.nextGaussian() * 5 to generate random normal variable with mean 0 sd 5
+var itis = iti_exponential(200, 700);  // intervals between dot-motion reps
 
 // dot motion task parameters
-dot_motion_repetitions = 3;
-dot_motion_deadline = 1500;
-p_incongruent_dots = 0.65;
-num_majority = 300;
+const dot_motion_repetitions = 3;
+const dot_motion_deadline = 1500;
+const p_incongruent_dots = 0.65;
+const num_majority = 300;
 
 // training block parameters
-num_reward_trials = 40;
-num_probe_trials = 20;
+const num_reward_trials = 40;
+const num_probe_trials = 20;
 
 // colours used for task, with left and right randomized for each experiment
-colours = ['#D00000', '#FF9505', '#6DA34D', '#3772FF'];
+// TODO orange and red might be too similar?!? (green/blue too??)
+var colours = ['#D00000', '#FF9505', '#6DA34D', '#3772FF'];  
 var colours = jsPsych.randomization.repeat(colours, 1);
-colours_left = colours.slice(2, 4)
-colours_right = colours.slice(0, 2)
+var colours_left = colours.slice(2, 4)
+var colours_right = colours.slice(0, 2)
 
 var subject_id = 1;
 var assigned_info = assign.filter(i => i.subject == subject_id)[0];
@@ -59,16 +63,17 @@ var instructions = {
     show_page_number: true,
 };
 
+// FIXME: left arrows are bigger and misaligned on chrome/safari (but okay on firefox)?
 var colour_blocks = {
     type: "html-keyboard-response",
     stimulus: `
-    <div style='width: 100px; float:left; padding-right: 10px;'>
-    <div style='color: ${colours_left[0]}; font-size:610%; margin-bottom: 20px; width: 100px; height: 100px; position: relative;'>&lArr;</div>
-    <div style='color: ${colours_left[1]}; font-size:610%; width: 100px; height: 100px; position: relative'>&lArr;</div>
+    <div style='width: 100px; float:left; padding-right: 55px;'>
+        <div style='color: ${colours_left[0]}; font-size:987%; margin-bottom: 55px; width: 100px; height: 100px; position: relative;'>&lArr;</div>
+        <div style='color: ${colours_left[1]}; font-size:987%; width: 100px; height: 100px; position: relative'>&lArr;</div>
     </div>
-    <div style='width: 100px; float:right; padding-left: 10px;'>
-    <div style='color: ${colours_right[0]}; font-size:610%; margin-bottom: 20px; width: 100px; height: 100px; position: relative;'>&rArr;</div>
-    <div style='color: ${colours_right[1]}; font-size:610%; width: 100px; height: 100px; position: relative'>&rArr;</div>
+    <div style='width: 100px; float:right; padding-left: 55px;'>
+        <div style='color: ${colours_right[0]}; font-size:987%; margin-bottom: 55px; width: 100px; height: 100px; position: relative;'>&rArr;</div>
+        <div style='color: ${colours_right[1]}; font-size:987%; width: 100px; height: 100px; position: relative'>&rArr;</div>
     </div>
   `
 }
@@ -79,8 +84,8 @@ var rockets = {
     type: "html-keyboard-response",
     stimulus: `
       <div>
-      <div style='float: left; padding-right: 10px'><img src='${images.rocket1}' width='100'></img></div>
-      <div style='float: right; padding-left: 10px'><img src='${images.rocket2}' width='100'></img></div>
+      <div style='float: left; padding-right: 10px'><img src='${images.rocket1}' width='233'></img></div>
+      <div style='float: right; padding-left: 10px'><img src='${images.rocket2}' width='233'></img></div>
       </div>
     `,
     choices: [37, 39],
@@ -97,14 +102,14 @@ var rockets = {
 
 var left_rocket_remaining =
     `<div>
-    <div style='float: left; padding-right: 10px'><img src='${images.rocket1}' width='100'></img></div>
-    <div style='float: right; padding-left: 10px'><img width='100'></img></div>
+    <div style='float: left; padding-right: 10px'><img src='${images.rocket1}' width='233'></img></div>
+    <div style='float: right; padding-left: 10px'><img width='233'></img></div>
     </div>`;
 
 var right_rocket_remaining =
     `<div>
-    <div style='float: left; padding-right: 10px'><img width='100'></img></div>
-    <div style='float: right; padding-left: 10px'><img src='${images.rocket2}' width='100'></img></div>
+    <div style='float: left; padding-right: 10px'><img width='233'></img></div>
+    <div style='float: right; padding-left: 10px'><img src='${images.rocket2}' width='233'></img></div>
     </div>`;
 
 var rocket_chosen = {
@@ -138,11 +143,12 @@ var dot_motion = {
     coherent_direction: function () { return dot_motion_parameters.coherent_direction },
     dot_color: function () { return [dot_motion_parameters.majority_col, dot_motion_parameters.distractor_col] },
     correct_choice: function () { return [dot_motion_parameters.correct_choice] },
-    move_distance: 6,
+    move_distance: 9,
     number_of_apertures: 2,
+    dot_radius: 2.5, // dot size (default 2)
     number_of_dots: function () { return [dot_motion_parameters.num_majority, dot_motion_parameters.num_distractors] },
-    RDK_type: 2,
-    aperture_width: 500,
+    RDK_type: 3,
+    aperture_width: 610,
     aperture_center_x: [(window.innerWidth / 2), (window.innerWidth / 2)],
     aperture_center_y: [(window.innerHeight / 2), (window.innerHeight / 2)],
     on_finish: function (data) {
@@ -157,12 +163,13 @@ var dot_motion = {
                 console.log('Your answer is incorrect')
             }
         }
-
         data.congruent = dot_motion_parameters.congruent;
-    }
+    },
+    post_trial_gap: function() {return random_choice(itis)}
 }
+// FIXME: weird that scrollbar shows up during dotmotion rep (see https://github.com/jspsych/jsPsych/discussions/787)
 
-// 1 dot motion trial
+// generate 1 dot motion trial
 function dot_motion_trial_variable(is_hard) {
     // select two random colours and assign them to answer and distractor
     var selected_colours = jsPsych.randomization.sampleWithoutReplacement(colours, 2)
@@ -238,10 +245,10 @@ var training_index = 0
 var cue = {
     type: "image-keyboard-response",
     stimulus: '',
-    stimulus_height: window.innerHeight/2,
+    stimulus_height: window.innerHeight / 2,
     maintain_aspect_ratio: true,
     on_start: function () {
-        document.body.style.backgroundImage = "url("+ training_timeline_variables[training_index].cue_image +")";
+        document.body.style.backgroundImage = "url(" + training_timeline_variables[training_index].cue_image + ")";
         document.body.style.backgroundSize = "cover";
     },
     on_finish: function (data) {
@@ -254,15 +261,15 @@ var cue = {
 var training_timeline_variables = get_training_timeline_variables(num_reward_trials, num_probe_trials, false);
 
 var training = {
-    timeline: [cue],
+    timeline: [cue], 
     timeline_variables: training_timeline_variables
 }
 
 
 var timeline = []
 // timeline.push(instructions);
-// timeline.push(colour_blocks);
-// timeline.push(pre_training);
+timeline.push(colour_blocks);
+timeline.push(pre_training);
 timeline.push(training);
 
 
