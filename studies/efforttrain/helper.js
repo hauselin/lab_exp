01a2769cbf32358077b0005a572824a9b5ef0e5a
frankafan <frank_fan_1201@outlook.com>
2021-05-29 15:51:44 -0700

    Changes during meeting.

--- studies/efforttrain/helper.js
+++ studies/efforttrain/helper.js
@@ -352,3 +352,86 @@ function calculate_points(rt, points_obj) {
     }
   }
 }
+
+// generate mental math updating array
+// determine correct response
+function number_update(array1, array2) {
+  var array_output = [];
+  var str_output = '';
+  for (i = 0; i < array1.length; i++) {
+      if (array2.length < array1.length) {
+          if (array2[0] > 9) throw "number in array2 must be < 10";
+          if (array2[0] < -9) throw "number in array2 must be > -10";
+          var correct_num = array1[i] + array2[0]; // if array2 is shorter than array1, always add the first element of array2 to each element in array1
+      } else if (array1.length == array2.length) {
+          if (array2[i] > 9) throw "number in array2 must be < 10";
+          if (array2[i] < -9) throw "number in array2 must be > -10";
+          var correct_num = array1[i] + array2[i];
+      };
+      if (correct_num < 0) {
+          correct_num += 10;
+      } else if (correct_num > 9) {
+          correct_num -= 10;
+      }
+      str_output = str_output.concat(correct_num.toString()); // concat string integers
+      array_output.push(correct_num);
+  }
+  return [array_output, str_output]
+}
+
+// create distractors/wrong responses
+function generate_similar_numbers(array, n_distractors) {
+  var result = [];
+  var v = 1; // distractor's difference from correct answer, changes with each additional distractor
+  while (result.length < n_distractors) { // loop stops when the result array is full
+      for (i = 0; i < array.length; i++) { // iterate through the different indeces of different distractors
+          result.push(array.slice(0, array.length));
+          result.push(array.slice(0, array.length)); // append two copies of the correct answer into the result
+          var y = array[i] // y is a copy of the correct answer's digit at different indeces
+          var y_plus = y + v;
+          var y_minus = y - v;
+          if (y_plus > 9) {
+              y_plus -= 10;
+          };
+          if (y_minus < 0) {
+              y_minus += 10;
+          };
+          result[result.length - 1][i] = y_plus;
+          result[result.length - 2][i] = y_minus; // the two copies undergo different changes at the same index.
+          // in the next iteration, newly pushed distractors change at the next index, but the same locations for the previous iteration's distractors do not change.
+      };
+      v += 1;
+  };
+  return [array].concat(shuffle(result.slice(0, n_distractors))); // [array + distractors]
+}
+
+// cue/prompt above each digit (string) (e.g., +3, -2)
+function update_prompt(digit) {
+  var s;
+  if (digit >= 0) {
+      s = "+" + digit;
+  } else {
+      s = "&#x2212;" + Math.abs(digit);  // minus sign
+  }
+  return s;
+}
+
+function process_choices(choices) {
+  var choices_copy = jsPsych.utils.deepCopy(choices);
+  var shuffled_options = [];
+  var options = generate_similar_numbers(number_update(temp_digits, [num_to_update])[0], n_distract_response);
+  options = options.map(x => x.join(''));
+  shuffled_options.push(
+      { prompt: options[0], correct: true }
+  );
+  for (i = 1; i < n_distract_response + 1; i++) {
+      shuffled_options.push(
+          { prompt: options[i], correct: false }
+      )
+  }
+  shuffled_options = shuffle(shuffled_options)
+  for (i = 0; i < n_distract_response + 1; i++) {
+      choices_copy[i] = Object.assign(choices_copy[i], shuffled_options[i]);
+  }
+  return choices_copy
+}
--- studies/efforttrain/task.js
+++ studies/efforttrain/task.js
@@ -688,88 +688,6 @@ if (n_distract_response == 3) {
     choices = choices.concat([{ keycode: 38, response: 'up' }, { keycode: 40, response: 'down' }]) // 3 distractors + 1 correct
 }
 
-// generate mental math updating array
-// determine correct response
-function number_update(array1, array2) {
-    var array_output = [];
-    var str_output = '';
-    for (i = 0; i < array1.length; i++) {
-        if (array2.length < array1.length) {
-            if (array2[0] > 9) throw "number in array2 must be < 10";
-            if (array2[0] < -9) throw "number in array2 must be > -10";
-            var correct_num = array1[i] + array2[0]; // if array2 is shorter than array1, always add the first element of array2 to each element in array1
-        } else if (array1.length == array2.length) {
-            if (array2[i] > 9) throw "number in array2 must be < 10";
-            if (array2[i] < -9) throw "number in array2 must be > -10";
-            var correct_num = array1[i] + array2[i];
-        };
-        if (correct_num < 0) {
-            correct_num += 10;
-        } else if (correct_num > 9) {
-            correct_num -= 10;
-        }
-        str_output = str_output.concat(correct_num.toString()); // concat string integers
-        array_output.push(correct_num);
-    }
-    return [array_output, str_output]
-}
-
-// create distractors/wrong responses
-function generate_similar_numbers(array, n_distractors) {
-    var result = [];
-    var v = 1; // distractor's difference from correct answer, changes with each additional distractor
-    while (result.length < n_distractors) { // loop stops when the result array is full
-        for (i = 0; i < array.length; i++) { // iterate through the different indeces of different distractors
-            result.push(array.slice(0, array.length));
-            result.push(array.slice(0, array.length)); // append two copies of the correct answer into the result
-            var y = array[i] // y is a copy of the correct answer's digit at different indeces
-            var y_plus = y + v;
-            var y_minus = y - v;
-            if (y_plus > 9) {
-                y_plus -= 10;
-            };
-            if (y_minus < 0) {
-                y_minus += 10;
-            };
-            result[result.length - 1][i] = y_plus;
-            result[result.length - 2][i] = y_minus; // the two copies undergo different changes at the same index.
-            // in the next iteration, newly pushed distractors change at the next index, but the same locations for the previous iteration's distractors do not change.
-        };
-        v += 1;
-    };
-    return [array].concat(shuffle(result.slice(0, n_distractors))); // [array + distractors]
-}
-
-// cue/prompt above each digit (string) (e.g., +3, -2)
-function update_prompt(digit) {
-    var s;
-    if (digit >= 0) {
-        s = "+" + digit;
-    } else {
-        s = "&#x2212;" + Math.abs(digit);  // minus sign
-    }
-    return s;
-}
-
-function process_choices(choices) {
-    var choices_copy = jsPsych.utils.deepCopy(choices);
-    var shuffled_options = [];
-    var options = generate_similar_numbers(number_update(temp_digits, [num_to_update])[0], n_distract_response);
-    options = options.map(x => x.join(''));
-    shuffled_options.push(
-        { prompt: options[0], correct: true }
-    );
-    for (i = 1; i < n_distract_response + 1; i++) {
-        shuffled_options.push(
-            { prompt: options[i], correct: false }
-        )
-    }
-    shuffled_options = shuffle(shuffled_options)
-    for (i = 0; i < n_distract_response + 1; i++) {
-        choices_copy[i] = Object.assign(choices_copy[i], shuffled_options[i]);
-    }
-    return choices_copy
-}
 
 var update_instructions1 = {
     type: "instructions",
@@ -795,12 +713,13 @@ var update_instructions2 = {
 var option1 = 0
 var option2 = 3
 
+// TODO: make hard and easy options' locations random
 var options = {
     type: "html-keyboard-response",
     stimulus: function () {
         option2 = 3;
         if (Math.random() < 0.5) {
-            option2 = 5;
+            option2 = 4;
         }
         var option2_str = "+" + String(option2)
         return generate_html("0", font_colour, 30, [-100, 25]) + generate_html(option2_str, font_colour, 30, [100, -25]);
@@ -847,8 +766,8 @@ var number_sequence = {
             type: "html-keyboard-response",
             stimulus: function () {
                 var remind = update_prompt(num_to_update);
-                remind = generate_html(remind, font_colour, 30) + "<br>";
-                var d = generate_html(jsPsych.timelineVariable('digit', true), font_colour, 80)
+                remind = generate_html(remind, font_colour, 30, [0, -50]) + "<br>";
+                var d = generate_html(jsPsych.timelineVariable('digit', true), font_colour, 80, [0, -50])
                 return remind + d;
             },
             choices: jsPsych.NO_KEYS,
@@ -940,17 +859,16 @@ for (i = 0; i < practice_sequence.timeline.length; i++) {
 
 var timeline = [];
 // timeline.push(instructions);
-timeline.push(colour_blocks);
-timeline.push(practice_hard_dot_trials);
+// timeline.push(colour_blocks);
+// timeline.push(practice_hard_dot_trials);
 // timeline.push(practice_easy_dot_trials);
 // timeline.push(practice_rocket_trials);
 // timeline.push(alien_introduction);
-// create timeline and updatemath events/objects for study (the first next lines are always the same!)
-// timeline.push(update_instructions1);
-// if (n_practice_update_trial > 0) {
-//     timeline.push(practice_sequence, update_instructions2);
-// }
-// timeline.push(trial_sequence);
+timeline.push(update_instructions1);
+if (n_practice_update_trial > 0) {
+    timeline.push(practice_sequence, update_instructions2);
+}
+timeline.push(trial_sequence);
 // timeline.push(practice_pre_training);
 // timeline.push(pre_training);
 // timeline.push(practice_training);
