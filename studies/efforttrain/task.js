d7b44819a7508a9afe04379be037a51422ffd71a
frankafan <frank_fan_1201@outlook.com>
2021-05-29 11:19:03 -0700

    Add updatemath task.

--- studies/efforttrain/task.js
+++ studies/efforttrain/task.js
@@ -556,13 +556,6 @@ var practice_rocket_trials = {
     repetitions: prac_rocket_max,
 }
 
-var timeline = [];
-timeline.push(instructions);
-timeline.push(colour_blocks);
-// timeline.push(practice_hard_dot_trials);
-// timeline.push(practice_easy_dot_trials);
-// timeline.push(practice_rocket_trials);
-
 // TODO: deepcopy pre-training for 5 trials of practice
 
 // timeline.push(pre_training);
@@ -592,13 +585,330 @@ var alien_introduction = {
     ],
 };
 
-timeline.push(alien_introduction);
-
 // TODO: training that only includes the reward alien / noreward alien, 5 trials each
 // TODO: proper training practice that includes both alien types, 4 each.
 // timeline.push(training);
 
 // TODO: post training, identical to pre-training, change event, do not store anything extra, store post-training to data.block
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DEFINE TASK PARAMETERS (required)
+var num_to_update = null; // number to add to every digit
+var n_digits = 3; // amount of numbers to show (must be > 1)
+var n_distract_response = 3; // amount of distractors
+var n_update_trial = 2; // number of trials and the amount of sequences to show
+var n_practice_update_trial = 1; // number of practice trials and the amount of sequences to show
+var duration_digit = 500; // how long to show each digit (ms)
+var duration_post_digit = 200;  // pause duration after each digit
+var update_feedback_duration = 1500;
+var rt_update_deadline = 3000;
+var options_deadline = null;
+
+if (debug) {
+    rt_update_deadline = 60000;
+}
+
+// keycode for responses
+var choices = [
+    { keycode: 37, response: 'left' },
+    { keycode: 39, response: 'right' },
+];
+if (n_distract_response == 3) {
+    choices = choices.concat([{ keycode: 38, response: 'up' }, { keycode: 40, response: 'down' }]) // 3 distractors + 1 correct
+}
+
+// generate mental math updating array
+// determine correct response
+function number_update(array1, array2) {
+    var array_output = [];
+    var str_output = '';
+    for (i = 0; i < array1.length; i++) {
+        if (array2.length < array1.length) {
+            if (array2[0] > 9) throw "number in array2 must be < 10";
+            if (array2[0] < -9) throw "number in array2 must be > -10";
+            var correct_num = array1[i] + array2[0]; // if array2 is shorter than array1, always add the first element of array2 to each element in array1
+        } else if (array1.length == array2.length) {
+            if (array2[i] > 9) throw "number in array2 must be < 10";
+            if (array2[i] < -9) throw "number in array2 must be > -10";
+            var correct_num = array1[i] + array2[i];
+        };
+        if (correct_num < 0) {
+            correct_num += 10;
+        } else if (correct_num > 9) {
+            correct_num -= 10;
+        }
+        str_output = str_output.concat(correct_num.toString()); // concat string integers
+        array_output.push(correct_num);
+    }
+    return [array_output, str_output]
+}
+
+// create distractors/wrong responses
+function generate_similar_numbers(array, n_distractors) {
+    var result = [];
+    var v = 1; // distractor's difference from correct answer, changes with each additional distractor
+    while (result.length < n_distractors) { // loop stops when the result array is full
+        for (i = 0; i < array.length; i++) { // iterate through the different indeces of different distractors
+            result.push(array.slice(0, array.length));
+            result.push(array.slice(0, array.length)); // append two copies of the correct answer into the result
+            var y = array[i] // y is a copy of the correct answer's digit at different indeces
+            var y_plus = y + v;
+            var y_minus = y - v;
+            if (y_plus > 9) {
+                y_plus -= 10;
+            };
+            if (y_minus < 0) {
+                y_minus += 10;
+            };
+            result[result.length - 1][i] = y_plus;
+            result[result.length - 2][i] = y_minus; // the two copies undergo different changes at the same index.
+            // in the next iteration, newly pushed distractors change at the next index, but the same locations for the previous iteration's distractors do not change.
+        };
+        v += 1;
+    };
+    return [array].concat(shuffle(result.slice(0, n_distractors))); // [array + distractors]
+}
+
+// cue/prompt above each digit (string) (e.g., +3, -2)
+function update_prompt(digit) {
+    var s;
+    if (digit >= 0) {
+        s = "+" + digit;
+    } else {
+        s = "&#x2212;" + Math.abs(digit);  // minus sign
+    }
+    return s;
+}
+
+function process_choices(choices) {
+    var choices_copy = jsPsych.utils.deepCopy(choices);
+    var shuffled_options = [];
+    var options = generate_similar_numbers(number_update(temp_digits, [num_to_update])[0], n_distract_response);
+    options = options.map(x => x.join(''));
+    shuffled_options.push(
+        { prompt: options[0], correct: true }
+    );
+    for (i = 1; i < n_distract_response + 1; i++) {
+        shuffled_options.push(
+            { prompt: options[i], correct: false }
+        )
+    }
+    shuffled_options = shuffle(shuffled_options)
+    for (i = 0; i < n_distract_response + 1; i++) {
+        choices_copy[i] = Object.assign(choices_copy[i], shuffled_options[i]);
+    }
+    return choices_copy
+}
+
+var update_instructions1 = {
+    type: "instructions",
+    pages: [
+        generate_html("Welcome!", font_colour) + generate_html("Click next or press the right arrow key to proceed.", font_colour),
+        generate_html("You have a limited amount of time to see each number, so react quickly!", font_colour),
+        generate_html("Next up is a practice trial.", font_colour) + generate_html("Your data will NOT be recorded.", font_colour) + generate_html("Click next or press the right arrow key to begin.", font_colour)
+    ],
+    show_clickable_nav: true,
+    show_page_number: true,
+};
+
+
+var update_instructions2 = {
+    type: "instructions",
+    pages: [
+        generate_html("That was the practice trial.", font_colour) + generate_html("Click next or press the right arrow key to begin the experiment.", font_colour) + generate_html("Your data WILL be recorded this time.", font_colour)
+    ],
+    show_clickable_nav: true,
+    show_page_number: false,
+};
+
+option1 = 0
+option2 = 3
+
+var options = {
+    type: "html-keyboard-response",
+    stimulus: function () {
+        option2 = Math.floor(Math.random() * 9) + 1;
+        option2_str = "+" + String(option2)
+        if (Math.random() < 0.5) {
+            option2 = - (Math.floor(Math.random() * 9) + 1);
+            option2_str = String(option2)
+        }
+        return generate_html("0", font_colour, 30, [-100, 25]) + generate_html(option2_str, font_colour, 30, [100, -25]);
+    },
+    choices: [37, 39],
+    trial_duration: options_deadline,
+    data: { event: "choice_options" },
+    post_trial_gap: 500,
+    on_finish: function (data) {
+        if (data.key_press == 37) { // pressed left
+            num_to_update = option1;
+        } else if (data.key_press == 39) { // pressed right
+            num_to_update = option2;
+        } else { // no response
+            num_to_update = null;
+        };
+        data.choice = num_to_update;
+        data.hard_choice = option2;
+        if (data.choice == data.hard_choice) {
+            data.percent_hard = 1;
+        } else {
+            data.percent_hard = 0;
+        }
+    }
+};
+
+var prompt_digit = {
+    type: "html-keyboard-response",
+    stimulus: function () {
+        var remind = update_prompt(num_to_update) + " to each digit";
+        remind = generate_html(remind, font_colour, 30);
+        return remind
+    },
+    choices: jsPsych.NO_KEYS,
+    trial_duration: 1000,
+    data: { event: "digit_prompt" },
+    post_trial_gap: 750
+};
+
+var temp_digits = [];
+var number_sequence = {
+    timeline: [
+        {
+            type: "html-keyboard-response",
+            stimulus: function () {
+                var remind = update_prompt(num_to_update);
+                remind = generate_html(remind, font_colour, 30) + "<br>";
+                var d = generate_html(jsPsych.timelineVariable('digit', true), font_colour, 80)
+                return remind + d;
+            },
+            choices: jsPsych.NO_KEYS,
+            trial_duration: duration_digit,
+            data: { event: "digit" },
+            post_trial_gap: duration_post_digit,
+            on_finish: function (data) {
+                data.digit = jsPsych.timelineVariable('digit', true);
+                temp_digits.push(data.digit);
+            }
+        }
+    ],
+    timeline_variables: Array.from(range(0, 10), x => Object({ digit: x })),
+    sample: { // pick different n_digits to present on each trial/sequence
+        type: 'with-replacement',
+        size: n_digits
+    }
+}
+
+var choices_shuffle;
+var update_response = {
+    type: "html-keyboard-response",
+    stimulus: function () {
+        choices_shuffle = process_choices(choices);
+        prompt_html = generate_html(choices_shuffle[0].prompt, font_colour, 30, [-100, 50]) + generate_html(choices_shuffle[1].prompt, font_colour, 30, [100, 2]);
+        if (n_distract_response == 3) {
+            prompt_html = prompt_html.concat(generate_html(choices_shuffle[2].prompt, font_colour, 30, [0, -100]) + generate_html(choices_shuffle[3].prompt, font_colour, 30, [0, -35]));
+        }
+        if (debug) {
+            console.log(choices_shuffle);
+        }
+        return prompt_html;
+    },
+    choices: choices.map(x => x.keycode),
+    trial_duration: rt_update_deadline,
+    data: { event: "response" },
+    post_trial_gap: 500,
+    on_finish: function (data) {
+        var chosen = choices_shuffle.filter(x => x.keycode == data.key_press)[0];
+        data.num_to_update = num_to_update;
+        if (!chosen) { // no response
+            data.acc = null;
+            data.response = null;
+        } else {  // response made
+            data.response = chosen.response;
+            if (chosen.correct) {
+                data.acc = 1;
+            } else {
+                data.acc = 0;
+            }
+        }
+        temp_digits = []; // clear digit sequences for next trial
+    }
+}
+
+var update_feedback = {
+    type: "html-keyboard-response",
+    stimulus: function () {
+        last_trial_data = jsPsych.data.getLastTrialData();
+        last_trial_value = last_trial_data.select('acc').values[0];
+        if (last_trial_value > 0) {
+            var prompt = "correct, your reaction time was " + Math.round(last_trial_data.select('rt').values[0]) + " ms";
+        } else if (last_trial_value === null) {
+            var prompt = "respond faster";
+        } else {
+            var prompt = "wrong";
+        }
+        return generate_html(prompt, font_colour, 25);
+    },
+    choices: jsPsych.NO_KEYS,
+    trial_duration: feedback_duration,
+    data: { event: "feedback" },
+    post_trial_gap: 500
+}
+
+var trial_sequence = {
+    timeline: [options, prompt_digit, number_sequence, update_response, update_feedback],
+    repetitions: n_update_trial,
+};
+
+var practice_sequence = jsPsych.utils.deepCopy(trial_sequence);
+practice_sequence.repetitions = n_practice_update_trial
+for (i = 0; i < practice_sequence.timeline.length; i++) {
+    practice_sequence.timeline[i].data = { event: "practice" }
+}
+
+
+
+
+var timeline = [];
+timeline.push(instructions);
+timeline.push(colour_blocks);
+// timeline.push(practice_hard_dot_trials);
+// timeline.push(practice_easy_dot_trials);
+// timeline.push(practice_rocket_trials);
+// timeline.push(alien_introduction);
+// create timeline and updatemath events/objects for study (the first next lines are always the same!)
+timeline.push(update_instructions1);
+if (n_practice_update_trial > 0) {
+    timeline.push(practice_sequence, update_instructions2);
+}
+timeline.push(trial_sequence);
 
 
 jsPsych.init({
