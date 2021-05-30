e754b3fdc799aa6d18132164d5ad809984c08ed5
hauselin <hauselin@gmail.com>
2021-05-29 21:14:03 -0600

    add instructions

--- studies/efforttrain/instructions.js
+++ studies/efforttrain/instructions.js
@@ -1,16 +1,102 @@
 // instruction text
-let instruct_browser = ["To ensure a seamless experience,<br>please use Chrome or Firefox web browser."]
+let instruct_browser = [
+  "To ensure a seamless experience,<br>please use Chrome or Firefox web browser.",
+];
 
-let instruct_intro = ["Welcome to our space station.<br><br>Whenever you're ready, click next or press the right arow key to proceed."]
+let instruct_intro = [
+  "Welcome to our space station.<br><br>Whenever you're ready, click next or press the right arow key on your keyboard to proceed.",
+];
 
 // same something about payment and bonus
-let instruct_mission1 = ["You are an adventurous space explorer who will complete several missions in space.<br><br>These missions will require about 1.5 hours of your time."]
+let instruct_mission1 = [
+  "You are an adventurous space explorer who will complete several missions in space.<br><br>These missions will require about <span style='color:orange; font-weight:bold'>1.5 hours</span> to complete.",
+];
 
-let instruct_mission2 = ["You'll have <span style='color:orange; font-weight:bold'>complete them in one setting</span>, so please ensure you feel well-rested and won't be disturbed in the next 1.5 hours.<br><br>Most of the time, you will be using the left/right arrow keys on your keyboard to complete the missions.<br><br>Press the right arrow key to continue."]
+let instruct_mission2 = [
+  "You'll have <span style='color:orange; font-weight:bold'>complete them in one setting</span>, so please ensure you feel well-rested and won't be disturbed in the next <span style='color:orange; font-weight:bold'>1.5 hours</span>.<br><br>Most of the time, you will be using the <span style='color:orange; font-weight:bold'>arrow keys</span> on your keyboard to complete the missions.",
+];
 
-let instruct_colortask = ["One mission is the <span style='color:orange; font-weight:bold'>star color-motion task</span>.<br><br>First, let's learn to identify the <span style='color:orange; font-weight:bold'>color</span> of the moving stars."]
+let instruct_colortask = [
+  "One mission is the <span style='color:orange; font-weight:bold'>star color-motion task</span>.<br><br>First, let's learn to identify the <span style='color:orange; font-weight:bold'>color</span> of the moving stars.",
+];
 
-let instruct_colortask2 = ["Sometimes you'll see stars in different colors.<br><br>Focus on identifying just the <span style='color:orange; font-weight:bold'>predominant color</span>. Which color appears most frequently?<br><br>Also, you'll see a rocket before seeing the stars. Whenever you see that rocket, it means you have to identify the <span style='color:orange; font-weight:bold'>color</span> of the stars.<br><br>Learn and remember that rocket."]
+let instruct_colortask2 = [
+  "You'll see stars in different colors.<br><br>Focus on identifying the <span style='color:orange; font-weight:bold'>predominant color</span>. Which color appears most frequently?<br><br>Also, you'll see a rocket before seeing the colored stars. Whenever you see that rocket, it means you have to identify the <span style='color:orange; font-weight:bold'>color</span> of the stars.<br><br>Memorize that rocket's appearance.",
+];
 
 // color blocks
-let instruct_colors = ["Two colors will be associated with the left key and another two colors with the right key."]
+const instruct_colors = (colours, hex2txt) => {
+  let i = 0;
+  let html = `If most of the stars are <span style='color:${
+    colours[i]
+  }; font-weight:bold'>${
+    hex2txt[colours[i]]
+  }</span>, press the <span style='color:${
+    colours[i]
+  }; font-weight:bold'>left arrow key</span> on your keyboard.<br>`;
+
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 144%'>
+  &lArr; &lArr; &lArr; &lArr;</span><br>`;
+
+  i++;
+  html += `If most of the stars are <span style='color:${
+    colours[i]
+  }; font-weight:bold'>${
+    hex2txt[colours[i]]
+  }</span>, also press the <span style='color:${
+    colours[i]
+  }; font-weight:bold'>left arrow key</span>.<br>`;
+
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 144%'>
+  &lArr; &lArr; &lArr; &lArr;</span><br><br><br>`;
+
+  i++;
+  html += `If most of the stars are <span style='color:${
+    colours[i]
+  }; font-weight:bold'>${
+    hex2txt[colours[i]]
+  }</span>, press the <span style='color:${
+    colours[i]
+  }; font-weight:bold'>right arrow key</span>.<br>`;
+
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 144%'>
+  &rArr; &rArr; &rArr; &rArr;</span><br>`;
+
+  i++;
+  html += `If most of the stars are <span style='color:${
+    colours[i]
+  }; font-weight:bold'>${
+    hex2txt[colours[i]]
+  }</span>, also press the <span style='color:${
+    colours[i]
+  }; font-weight:bold'>right arrow key</span>.<br>`;
+
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 144%'>
+  &rArr; &rArr; &rArr; &rArr;</span><br>`;
+
+  return [html];
+};
+
+// color blocks
+const colors_remind = (colours, remind_txt) => {
+  let i = 0;
+
+  let html = remind_txt + "<br><br>";
+
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size:233%'>
+  &lArr; &lArr; &lArr; &lArr;</span><br><br>`;
+
+  i++;
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 233%'>
+  &lArr; &lArr; &lArr; &lArr;</span><br><br>`;
+
+  i++;
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 233%'>
+  &rArr; &rArr; &rArr; &rArr;</span><br><br>`;
+
+  i++;
+  html += `<span style='color:${colours[i]}; font-weight:bold; font-size: 233%'>
+  &rArr; &rArr; &rArr; &rArr;</span><br><br>`;
+
+  return [html];
+};
