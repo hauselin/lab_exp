157bdcdc7d13515c895d274cf0c0a9d89c9eb555
frankafan <frank_fan_1201@outlook.com>
2021-06-20 15:40:59 -0700

    Add file name.

--- studies/efforttrain/assign.py
+++ studies/efforttrain/assign.py
@@ -8,9 +8,9 @@ num_patterns = 6
 rockets = []
 patterns = []
 for r in range(num_rockets):
-    rockets.append('rocket' + f"{r+1:02}")
+    rockets.append('rocket' + f"{r+1:02}" + '.png')
 for p in range(num_patterns):
-    patterns.append('pattern' + f"{p+1:02}")
+    patterns.append('pattern' + f"{p+1:02}" + '.png')
 
 rocket_permutations = list(itertools.permutations(rockets, 2))
 pattern_permutations = list(itertools.permutations(patterns, 2))
