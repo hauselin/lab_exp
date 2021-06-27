135b4fea799f4780aa183f0635a245b7ebf7855d
frankafan <frank_fan_1201@outlook.com>
2021-06-26 21:16:41 -0700

    Do not assign rockets and patterns.

--- studies/efforttrain/assign.py
+++ studies/efforttrain/assign.py
@@ -2,18 +2,18 @@ import itertools
 import csv
 import json
 
-num_rockets = 6
-num_patterns = 6
+# num_rockets = 6
+# num_patterns = 6
 
-rockets = []
-patterns = []
-for r in range(num_rockets):
-    rockets.append('rocket' + f"{r+1:02}" + '.jpg')
-for p in range(num_patterns):
-    patterns.append('pattern' + f"{p+1:02}" + '.jpg')
+# rockets = []
+# patterns = []
+# for r in range(num_rockets):
+#     rockets.append('rocket' + f"{r+1:02}" + '.jpg')
+# for p in range(num_patterns):
+#     patterns.append('pattern' + f"{p+1:02}" + '.jpg')
 
-rocket_permutations = list(itertools.permutations(rockets, 2))
-pattern_permutations = list(itertools.permutations(patterns, 2))
+# rocket_permutations = list(itertools.permutations(rockets, 2))
+# pattern_permutations = list(itertools.permutations(patterns, 2))
 
 train_types = ['dotmotion', 'update']
 pretrain_orders = list(itertools.permutations(train_types, 2))
@@ -43,15 +43,15 @@ for perm in colour_permutations:
 
 subjects = []
 for c in range(len(colour_code_permutations)):
-    for r in range(len(rocket_permutations)):
-        for p in range(len(pattern_permutations)):
+    # for r in range(len(rocket_permutations)):
+    #     for p in range(len(pattern_permutations)):
             for pre in range(len(pretrain_orders)):
                 for post in range(len(posttrain_orders)):
                     subject = {
-                        'rocket_easy': rocket_permutations[r][0],
-                        'rocket_hard': rocket_permutations[r][1],
-                        'pattern_easy': pattern_permutations[p][0],
-                        'pattern_hard': pattern_permutations[p][1],
+                        # 'rocket_easy': rocket_permutations[r][0],
+                        # 'rocket_hard': rocket_permutations[r][1],
+                        # 'pattern_easy': pattern_permutations[p][0],
+                        # 'pattern_hard': pattern_permutations[p][1],
                         'colours_hex': colour_code_permutations[c],
                         'colours_name': colour_permutations[c],
                         'pretrain_order': '-'.join(pretrain_orders[pre]),
@@ -64,13 +64,18 @@ print(len(subjects))
 
 with open('assign.csv', mode='w') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['rocket_easy', 'rocket_hard', 'pattern_easy', 'pattern_hard', 'colours_hex', 'colours_name', 'pretrain_order', 'posttrain_order'])
+    writer.writerow([
+        # 'rocket_easy', 
+        # 'rocket_hard', 
+        # 'pattern_easy', 
+        # 'pattern_hard', 
+        'colours_hex', 'colours_name', 'pretrain_order', 'posttrain_order'])
     for subject in subjects:
         writer.writerow([
-            subject['rocket_easy'],
-            subject['rocket_hard'],
-            subject['pattern_easy'],
-            subject['pattern_hard'],
+            # subject['rocket_easy'],
+            # subject['rocket_hard'],
+            # subject['pattern_easy'],
+            # subject['pattern_hard'],
             subject['colours_hex'],
             subject['colours_name'],
             subject['pretrain_order'],
