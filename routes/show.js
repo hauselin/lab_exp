9a92ddc3dbef6fa2053063b5817331f32fd38672
frankafan <frank_fan_1201@outlook.com>
2021-06-26 22:00:32 -0700

    Fix async function issue using promise.

--- routes/show.js
+++ routes/show.js
@@ -15,37 +15,45 @@ router.get("/:type/:uniquestudyid", function (req, res, next) {
 
   // TODO if has query parameter "count=true", query database to count no. of items counter (else don't query)
   // http://localhost:8080/surveys/gritshort/?counter=true
-  let counter = 0;
-  if (req.query.count) {
-    DataLibrary.countDocuments({ uniquestudyid: req.params.uniquestudyid }, function(err, count){
-      console.log( "Number of docs: ", count );
-      // count=true exist in query string
-      console.log("query database");
-      counter = count;
-    });
-  }
-
-  // check if html static file exists without opening it
-  fs.access(root + "/" + "task.html", fs.F_OK, (err) => {
-    if (err) {
-      if (root.endsWith(".map")) {
-        // catch weird issue with consent forms
-        res.sendStatus(100);
-      } else {
-        console.log("Route not found. Moving to next route.");
-        helper.cssFix(req, res, "comingSoon", 200);
-        // next(); // if file doesn't exist, go to next route
-      }
-    } else {
-      // TODO query datalibrary here
-      console.log(root + "/" + "task.html");
-      res.sendFile("task.html", {
-        root: root,
-        headers: {
-          "X-counter": counter,
-        },
+  var promise = new Promise(function(resolve, reject) {
+    if (req.query.count) {
+      DataLibrary.countDocuments({ uniquestudyid: req.params.uniquestudyid }, function(err, count){
+        console.log( "Number of docs: ", count );
+        // count=true exist in query string
+        console.log("query database");
+        resolve(count);
       });
+    } else {
+      resolve(0);
     }
+  });
+  promise.then(function(value) {
+      counter=value;
+  }).then(function() {
+      console.log(counter)
+  }).then(function() {
+    // check if html static file exists without opening it
+    fs.access(root + "/" + "task.html", fs.F_OK, (err) => {
+      if (err) {
+        if (root.endsWith(".map")) {
+          // catch weird issue with consent forms
+          res.sendStatus(100);
+        } else {
+          console.log("Route not found. Moving to next route.");
+          helper.cssFix(req, res, "comingSoon", 200);
+          // next(); // if file doesn't exist, go to next route
+        }
+      } else {
+        // TODO query datalibrary here
+        console.log(root + "/" + "task.html");
+        res.sendFile("task.html", {
+          root: root,
+          headers: {
+            "X-counter": counter,
+          },
+        });
+      }
+    });
   });
 });
 
